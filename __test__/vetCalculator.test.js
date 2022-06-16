f7bf8b23bebdb1aed3fd81f0bdc081688c43974b
Fabjoo1 <fkoci@unitsystem.net>
2022-06-16 16:25:35 +0200

    commiting vetCalculator : VAT and GroosAmmount

--- __test__/vetCalculator.test.js
+++ __test__/vetCalculator.test.js
@@ -1,12 +1,14 @@
 const vatCalculator = require('../src/utils/vatCalculator')
 
 describe("VAT calculator" , ()=>{
-    test("Should return the correct VAT excluded amount for 20% " , ()){
-    const result = vatCalculator.calculateVAT(16.67)
-    except(result).toBe(3.33)
+    test("Should return the correct VAT excluded amount for 20% VAT " , () =>{
+      const result = vatCalculator.calculateVAT(16.67)
+      except(result).toBe(3.33)
+    })
+    
+    test("Should return correct gross amount of 20% VAT", ()=>{
+        const result = vatCalculator.calculateGroosAmmount(16.67)
+        except(result).toBe(20)
     })
 
-    test("Should return correct gross ammount of 20% VAT ", ()=>{
-        
-    })
 })

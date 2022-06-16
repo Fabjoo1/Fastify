a6936431ada118c2d962389133771ef6279a2712
Fabjoo1 <fkoci@unitsystem.net>
2022-06-16 15:03:20 +0200

    app- adding optsPost/swagg on build

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const fastify = require('fastify')
 const fastifySwagger = require('@fastify/swagger')
+const fastifyPostgres= require('@fastify/postgres')
+
 
 const {itemRuote}=require('./routes/items')
  
 
-const build = (opts={})=>{
-    const app= fastify(opts)
+const build = (opts={} , optsPostgres={}, optsSwagger={})=>{
+    const app= fastify(opts, optsPostgres)
+    app.register(fastifyPostgres,optsPostgres)
     app.register(fastifySwagger,optsSwagger)
     app.register(toMake)
     return app

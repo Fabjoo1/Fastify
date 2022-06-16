6475d96eebc1ab47a2ae7e7f3d5a935ca293b4d3
Fabjoo1 <fkoci@unitsystem.net>
2022-06-16 15:25:29 +0200

    adding DB test and fixing server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,28 @@ const {build} = require('./app')
 
 const app = build(
     {logger:true},
-    {exposeRuote: true , ruotePrefix : "./docs" , swagger : {info: {tittle: "Fastify API" , }}}
-
-
-    )    
-
+    {exposeRuote: true , 
+    ruotePrefix : "./docs" , 
+    swagger : {info: {tittle: "Fastify API" , version : "1.0.0" }}},
+    {
+        connectionString : 'postgres://postgres:postgres@localhost:5432/postgres'
+    });
+    
+    
+app.get('./time' ,(req, reply)=>{
+    app.pg.connect(onConnect)
+    function onConnect (err, client, relase){
+        if (err) return reply.send(err)
+        
+        client.query(
+            'SELECT now()',
+            function onResult (err, result){
+                relase()
+                reply.send(err|| result.row[0])
+            }
+        )
+    }
+} )
 
 
 app.listen(3000, function(err, address){
